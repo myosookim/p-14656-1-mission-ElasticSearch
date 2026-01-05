a49ce9bc1baa09268400b2ebd95335395cb10538
myosookim <o_rul8_2@naver.com>
2026-01-05 15:31:46 +0900

    0008

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,9 +23,12 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-webmvc")
     testImplementation("org.springframework.boot:spring-boot-starter-data-elasticsearch-test")
     testImplementation("org.springframework.boot:spring-boot-starter-webmvc-test")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
     // 애플리케이션 실행 시, 스프링부트가 Docker Compose를 자동으로 감지하고 필요한 인프라(DB, Redis, Kafka 등)를 알아서 띄워주게 해줌
     runtimeOnly("org.springframework.boot:spring-boot-docker-compose")
-    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    // Lombok
+    compileOnly("org.projectlombok:lombok")
+    annotationProcessor("org.projectlombok:lombok")
 }
 
 tasks.withType<Test> {
--- src/main/java/com/back/global/initData/BaseInitData.java
+++ src/main/java/com/back/global/initData/BaseInitData.java
@@ -1,17 +1,19 @@
 package com.back.global.initData;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration  // 설정 클래스 지정
+@Slf4j
 public class BaseInitData {
 
     // Bean을 통해 애플리케이션 시작 시 초기화 로직 실행
     @Bean
     public ApplicationRunner baseInitDataRunner (){
         return args->{
-            System.out.println("ApplicationRunner 빈은 스프링에 등록되면 자동으로 실행됩니다");
+            log.debug("ApplicationRunner 빈은 스프링에 등록되면 자동으로 실행됩니다.");
         };
     }
 }
